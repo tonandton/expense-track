2fc744ae0c068445ffe4cf5a30c6d218f2b19447
tonandton <www.sorasakok@gmail.com>
2025-03-02 18:20:56 +0700

    update mongo db and routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
 const app = express();
 
 require("dotenv").config();
@@ -10,10 +12,20 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {});
+// routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 const server = () => {
-  console.log("You are listening to port: ", PORT);
+  db();
+  app.listen(PORT, () => {
+    console.log("You are listening to port: ", PORT);
+  });
 };
 
 server();
