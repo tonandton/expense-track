788104f9f40509ae65192d25f6a1b2ddb64c2f9e
tonandton <www.sorasakok@gmail.com>
2025-03-02 21:00:24 +0700

    update model income

---
+++ backend/routes/transactions.js
@@ -0,0 +1,14 @@
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
+
+const router = require("express").Router();
+
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome);
+
+module.exports = router;
