61e30ca87982cbdf8db521c516af080486f27d4d
tonandton <www.sorasakok@gmail.com>
2025-03-04 22:39:19 +0700

    update bg style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 import bg from "./img/bg.png";
+import Orb from "./components/Orb/Orb";
 import { MainLayout } from "./styles/Layout";
 
 function App() {
   return (
     <AppStyled bg={bg} className="App">
-      <MainLayout>
-        <h1>asds;aALSK</h1>
-      </MainLayout>
+      <Orb />
+      <MainLayout></MainLayout>
     </AppStyled>
   );
 }
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${(props) => props.bg});
+  /* background-image: url(${(props) => props.bg}); */
+  position: relative;
 `;
 
 export default App;
