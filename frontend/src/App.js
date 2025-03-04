969f11153e3ce863ed997582713ec4f1093d3865
tonandton <www.sorasakok@gmail.com>
2025-03-04 21:53:54 +0700

    front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layout";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppStyled bg={bg} className="App">
+      <MainLayout>
+        <h1>asds;aALSK</h1>
+      </MainLayout>
+    </AppStyled>
+  );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+`;
 
 export default App;
