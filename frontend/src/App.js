adcbd6a8befa904d91242563aa34f4b3d160f3f2
tonandton <www.sorasakok@gmail.com>
2025-03-06 00:31:27 +0700

    update prorfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
 const AppStyled = styled.div`
   height: 100vh;
   /* background-image: url(${(props) => props.$bg}); */
+  background: #3333;
   position: relative;
 `;
 
--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -2,14 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import profile from "../../img/profile.png";
 import { menuItems } from "../../utils/menuItems";
+import { GoSignOut } from "react-icons/go";
 
 function Navigation() {
   return (
     <NavStyled>
       <div className="user-con">
         <img src={profile} alt="" />
-        <h2>BB</h2>
-        <p>Your Money</p>
+        <div>
+          <h2>BBBB</h2>
+          <p>Your Money</p>
+        </div>
       </div>
 
       <ul className="menu-items">
@@ -22,11 +25,60 @@ function Navigation() {
           );
         })}
       </ul>
-      <div className="bottom-nav">{/* <li>{signout} Sign Out</li> */}</div>
+      <div className="bottom-nav">
+        <li>
+          <GoSignOut /> Sign Out
+        </li>
+      </div>
     </NavStyled>
   );
 }
 
-const NavStyled = styled.nav``;
+const NavStyled = styled.nav`
+  padding: 2rem 1.5rem;
+  width: 374px;
+  height: 100%;
+  background: rgba(252, 246, 249, 0.78);
+  backdrop-filter: blur(4.5px);
+  border: 3px solid #ffffff;
+  border-radius: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+
+  .user-con {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      object-fit: cover;
+      background: #fcf6f9;
+      border: 2px solid #ffffff;
+      padding: 0.2rem;
+      box-shadow: 0px 1px 17px rgba(0, 0, 0, 0.06);
+    }
+    h2 {
+      color: rgba(34, 34, 96, 1);
+    }
+    p {
+      color: rgba(34, 34, 96, rgba(0, 0, 0, 0.06));
+    }
+  }
+
+  .menu-items {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    li {
+      display: grid;
+      grid-template-columns: 40px auto;
+    }
+  }
+`;
 
 export default Navigation;
